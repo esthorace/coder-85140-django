1842fad24bf3a9ecfff878d7146f7cbf1729ed68
Esteban <esteban.horacio.acevedo@gmail.com>
2026-02-11 21:00:21 -0300

    Registra modelos en admin

--- src/prueba/models.py
+++ src/prueba/models.py
@@ -1,13 +1,31 @@
+from tabnanny import verbose
+
 from django.db import models
 
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
 
+    def __str__(self) -> str:
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Países"
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     pais_origen = models.ForeignKey(
-        Pais, on_delete=models.SET_NULL, null=True, blank=True
+        Pais,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="País de origen",
     )
+
+    def __str__(self) -> str:
+        return f"{self.apellido.upper()}, {self.nombre}"
+
+    class Meta:
+        verbose_name_plural = "Clientes"
